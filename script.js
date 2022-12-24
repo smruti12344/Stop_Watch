ee3f644998c519afc80e8518d384b621a0fcbd8c
smruti12344 <72293400+smruti12344@users.noreply.github.com>
2022-12-24 20:16:06 +0530

    script file

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ let stopBtn = document.getElementById('stop');
 let resetBtn = document.getElementById('reset');
 
 // here intialize the value of timer obj
-//  let hour = 00;
+ let hour = 00;
  let minute = 00;
  let second = 00;
- let count = 00;
+//  let count = 00;
 
 //  here used function for start timer
 
@@ -27,45 +27,46 @@ stopBtn.addEventListener('click',function(){
 
 resetBtn.addEventListener('click',function(){
     timer=false;
-    // hour= 0;
+    hour= 0;
     minute= 0;
     second= 0;
-    count= 0;
+    // count= 0;
 
-    // document.getElementById('hr').innerHTML= "00";
+    document.getElementById('hr').innerHTML= "00";
     document.getElementById('min').innerHTML= "00";
     document.getElementById('sec').innerHTML= "00";
-    document.getElementById('count').innerHTML= "00";
+    // document.getElementById('count').innerHTML= "00";
 });
 // here used function to perform 
 
 function stopWatch(){
      if(timer){
-        count++;
+        // count++;
+        second++;
 
-        if(count== 100){
-            second++;
-            count= 0;
-        }
+        // if(count== 100){
+        //     second++;
+        //     count= 0;
+        // }
         if(second== 60){
             minute++;
             second= 0;
         }
-        // if(minute==60){
-        //     hour++;
-        //     minute=0;
-        //     second=0;
-        // }
+        if(minute==60){
+            hour++;
+            minute=0;
+            second=0;
+        }
 
-    // let Counthr = hour;
+    let Counthr = hour;
     let countmin =minute;
     let countsec = second;
-    let countCounter= count;
+    // let countCounter= count;
 // here condition for single digit number show
 
-    //  if(hour<10){
-    //     Counthr = "0" + Counthr;
-    //  }
+     if(hour<10){
+        Counthr = "0" + Counthr;
+     }
 
      if(minute<10){
         countmin = "0" + countmin;
@@ -75,16 +76,16 @@ function stopWatch(){
         countsec = "0" + countsec;
      }
 
-     if(count<10){
-        countCounter = "0" + countCounter;
-     }
+    //  if(count<10){
+    //     countCounter = "0" + countCounter;
+    //  }
 //   here  access element to show count
 
     //  document.getElementById('hr').innerHTML= Counthr;
     document.getElementById('min').innerHTML= countmin;
     document.getElementById('sec').innerHTML= countsec;
-    document.getElementById('count').innerHTML= countCounter;
-    setTimeout(stopWatch,10);
+    // document.getElementById('count').innerHTML= countCounter;
+    setTimeout(stopWatch,1000);
 
      }
 }
